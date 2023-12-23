4c6643142784e3a49235926da92470867b1d59e8
Steve Bradshaw <steve@pcfive.co.uk>
2023-12-23 14:34:08 +0000

    Mysql category query

--- src/oscar/apps/catalogue/expressions.py
+++ src/oscar/apps/catalogue/expressions.py
@@ -8,7 +8,7 @@ LEFT JOIN "catalogue_category" AS "CATALOGUE_CATEGORY_JOIN" ON (
 )
 WHERE "CATALOGUE_CATEGORY_BASE"."id" IN (%(subquery)s))
 """
-
+EXPAND_UPWARDS_CATEGORY_QUERY_MYSQL = EXPAND_UPWARDS_CATEGORY_QUERY.replace('"', '`')
 
 EXPAND_DOWNWARDS_CATEGORY_QUERY = """
 (SELECT "CATALOGUE_CATEGORY_JOIN"."id" FROM "catalogue_category" AS "CATALOGUE_CATEGORY_BASE"
@@ -18,11 +18,18 @@ LEFT JOIN "catalogue_category" AS "CATALOGUE_CATEGORY_JOIN" ON (
 )
 WHERE "CATALOGUE_CATEGORY_BASE"."id" IN (%(subquery)s))
 """
+EXPAND_DOWNWARDS_CATEGORY_QUERY_MYSQL = EXPAND_DOWNWARDS_CATEGORY_QUERY.replace('"', '`')
 
 
 # pylint: disable=abstract-method
 class ExpandUpwardsCategoryQueryset(Subquery):
     template = EXPAND_UPWARDS_CATEGORY_QUERY
+    mysql_template = EXPAND_UPWARDS_CATEGORY_QUERY_MYSQL
+
+    def as_sql(self, compiler, connection, template=None, **extra_context):
+        if connection.vendor == "mysql":
+            return super().as_sql(compiler, connection, self.mysql_template, **extra_context)
+        return super().as_sql(compiler, connection, self.template, **extra_context)
 
     def as_sqlite(self, compiler, connection):
         return super().as_sql(compiler, connection, self.template[1:-1])
@@ -31,6 +38,12 @@ class ExpandUpwardsCategoryQueryset(Subquery):
 # pylint: disable=abstract-method
 class ExpandDownwardsCategoryQueryset(Subquery):
     template = EXPAND_DOWNWARDS_CATEGORY_QUERY
+    mysql_template = EXPAND_DOWNWARDS_CATEGORY_QUERY_MYSQL
+
+    def as_sql(self, compiler, connection, template=None, **extra_context):
+        if connection.vendor == "mysql":
+            return super().as_sql(compiler, connection, self.mysql_template, **extra_context)
+        return super().as_sql(compiler, connection, self.template, **extra_context)
 
     def as_sqlite(self, compiler, connection):
         return super().as_sql(compiler, connection, self.template[1:-1])
